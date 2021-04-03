34c6e600c9f472a3decb38ec90cd1dcd85b73497
Carl Benbow <cbenbow@synergisticagility.com>
2021-04-03 10:23:08 -0400

    updated css file tp style html2, added apis tp js

--- js/recipe_mixer.js
+++ js/recipe_mixer.js
@@ -1,38 +1,80 @@
-//  $(document).ready(function(){
+$(document).ready(function(){
 
-    fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=chicken&ignorePantry=true", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "f86279dabamshfff699b5628a387p1294c5jsn02d42833bd7d",
-            "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-        }
-    })
-    .then(response => {
-        console.log(response);
-        return response.json();
-    })
-     .then(data=> {
+  var diet =""; //vegan, paleo etc
+  var incredients =""; //incredients (comma seperate)
+  
+
+  fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?includeIngredients=chicken,tomatoes", {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-key": "f86279dabamshfff699b5628a387p1294c5jsn02d42833bd7d",
+      "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+    }
+  })
+  .then(response => {
+    console.log(response);
+    return response.json();
+  })
+  .then(data=> {
        console.log(data);
-      $(".title1").text(data[0].title);
-      $(".title2").text(data[1].title);
-      $(".title3").text(data[2].title);
-      $(".title4").text(data[3].title);
-    //  $(".resultTwoHeader").text(data.array[1].title);
-    //  $(".resultThreeHeader").text(data.array[2].title);
-    //  $(".resultFourHeader").text(data.array[3].title);
-     
-    //  $(".weatherIconCurrent").text(data.weather.icon); 
-    //  $(".temperatureCurrent").text("Temperature: " +data.main.temp +"");
-    //  $(".humidityCurrent").text("Humidity: "+data.main.humidity+"%");
-    //  $(".uvIndexCurrent").text(data.name);
-    //  $(".windSpeedCurrent").text("Wind Speed: "+data.wind.speed+" MPH");
+      $(".title1").text(data.results[0].title);
+      $(".img1").attr("src",(data.results[0].image));
+      // $(".title2").text(data[0].
+      $(".title2").text(data.results[1].title);
+      $(".img2").attr("src",(data.results[1].image));
+      $(".title3").text(data.results[2].title);
+      $(".img3").attr("src",(data.results[2].image))
+      $(".title4").text(data.results[2].title);
+      $(".img4").attr("src",(data.results[2].image));
+
+
      })
     .catch(err => {
         console.error(err);
     });
 
- 
 
+
+
+
+
+    // fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=fish&ignorePantry=true", {
+    //     "method": "GET",
+    //     "headers": {
+    //         "x-rapidapi-key": "f86279dabamshfff699b5628a387p1294c5jsn02d42833bd7d",
+    //         "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+    //     }
+    // })
+    // .then(response => {
+    //     console.log(response);
+    //     return response.json();
+    // })
+    //  .then(data=> {
+    //    console.log(data);
+    //   $(".title1").text(data[0].title);
+    //   $(".img1").attr("src",(data[0].image));
+    //   // $(".title2").text(data[0].
+    //   $(".title2").text(data[1].title);
+    //   $(".img2").attr("src",(data[1].image));
+    //   $(".title3").text(data[2].title);
+    //   $(".img3").attr("src",(data[2].image));
+    //   $(".title4").text(data[3].title);
+    //   $(".img4").attr("src",(data[3].image));
+
+
+    //  })
+    // .catch(err => {
+    //     console.error(err);
+    // });
+
+ 
+// $(".btn").eventListener("click",function(event)){
+//   event.target
+// }
+$(".btn").click(function(){
+   var  getRecipe = $(this).parent().parent().prev().text()
+   console.log(getRecipe)
+});
 
 
 
@@ -44,40 +86,13 @@
       } );
 
       $( "#accordion" ).accordion({
-        heightStyle: "fill"
+        heightStyle: "content"
       });
+      
 
-// });
+});
   
-//     var apiKey = "92082348e1fdf20edfb9752ebc60f8bc"
-//     var lat = 0
-//     var long = 0 
-//     var city = "boston"
-  
-//     function getCurrentWeather(){
-     
-//     var requestUrl ='https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+ apiKey;
-      
-//       fetch(requestUrl)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//        $(".state-results").text(data.name);
-//        $(".weatherIconCurrent").text(data.weather.icon); 
-//        $(".temperatureCurrent").text("Temperature: " +data.main.temp +"");
-//        $(".humidityCurrent").text("Humidity: "+data.main.humidity+"%");
-//        $(".uvIndexCurrent").text(data.name);
-//        $(".windSpeedCurrent").text("Wind Speed: "+data.wind.speed+" MPH");
-//        lat = data.coord.lat
-//        long = data.coord.lon
-       
-//     });
-      
-//   }
-  
-// }
+
   
 
 //Carl Results section add code
